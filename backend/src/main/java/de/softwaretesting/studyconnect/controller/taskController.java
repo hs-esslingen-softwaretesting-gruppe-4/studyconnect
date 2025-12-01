d0206e3e8e8afd53269e63a4b8270c15c0fa4720
Leonhard Schneider <lescit07@hs-esslingen.de>
2025-12-01 12:17:48 +0100

    feat: refactor taskController to use TaskService for task operations and improve response handling

--- backend/src/main/java/de/softwaretesting/studyconnect/controller/taskController.java
+++ backend/src/main/java/de/softwaretesting/studyconnect/controller/taskController.java
@@ -1,11 +1,12 @@
 package de.softwaretesting.studyconnect.controller;
 
 import de.softwaretesting.studyconnect.repositories.*;
-import jakarta.validation.Valid;
-import de.softwaretesting.studyconnect.models.*;
+import de.softwaretesting.studyconnect.services.TaskService;
+import de.softwaretesting.studyconnect.dtos.request.TaskRequestDTO;
+import de.softwaretesting.studyconnect.dtos.response.TaskResponseDTO;
+import de.softwaretesting.studyconnect.exceptions.NotFoundException;
 
 import org.springframework.http.ResponseEntity;
-import org.hibernate.cache.spi.support.AbstractReadWriteAccess.Item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -16,12 +17,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -31,66 +29,57 @@ public class taskController {
     @Autowired
     TaskRepository taskRepository;
 
-    @GetMapping("/tasks")
-    public ResponseEntity<List<Task>> getAllTasks(@RequestParam(required = false) Long groupId) {
-        try {
-            List<Task> tasks = new ArrayList<Task>();
-            tasks = taskRepository.findByGroupId(groupId);
-            if (tasks.isEmpty()) {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
+    @Autowired
+    TaskService TaskService;
 
-            return new ResponseEntity<>(tasks, HttpStatus.OK);
+    @GetMapping("/tasks/groups/{groupId}")
+    public ResponseEntity<List<TaskResponseDTO>> getAllTasksByGroupId(@PathVariable("groupId") Long groupId) {
+        try {
+            return TaskService.getAllTasksInGroup(groupId);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    @GetMapping("/tasks/{id}")
-    public ResponseEntity<Task> getTaskById(@PathVariable("id") Long id) {
-        Optional<Task> taskData = taskRepository.findById(id);
-        if (taskData.isPresent()) {
-            return new ResponseEntity<>(taskData.get(), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    @GetMapping("/tasks/users/{userId}")
+    public ResponseEntity<List<TaskResponseDTO>> getTaskById(@PathVariable("id") Long userId) {
+        try {
+            return TaskService.getAllTasksAssignedToUser(userId);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @PostMapping("/tasks")
-    public ResponseEntity<Task> createTask(@RequestBody Task task) {
+    public ResponseEntity<TaskResponseDTO> createTask(@RequestBody Long groupId, TaskRequestDTO task) {
         try {
-            Task _task = taskService.createEntity(task);
-            return new ResponseEntity<>(_task, HttpStatus.CREATED);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+            return TaskService.createTask(groupId, task);
+        } catch (NotFoundException n) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-	@PutMapping("/tasks/{id}")
-	public ResponseEntity<Task> updateTask(@PathVariable("id") long id, @RequestBody Task task) {
-		Optional<Task> taskData = taskService.findById(id);
-
-		if (taskData.isPresent()) {
-			Task _task = taskData.get();
-            _task.setTitle(task.getTitle());
-			_task.setDescription(task.getDescription());
-            _task.setDueDate(task.getDueDate());
-            _task.setPriority(task.getPriority());
-            _task.setCategory(task.getCategory());
-            _task.setStatus(task.getStatus());
-			return new ResponseEntity<>(taskService.saveNReturn(_task), HttpStatus.OK);
-		} else {
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	@PutMapping("/tasks/{taskId}")
+	public ResponseEntity<TaskResponseDTO> updateTask(@PathVariable("taskId") Long taskId, @RequestBody TaskRequestDTO task) {
+		try {
+            return TaskService.updateTask(taskId, task);
+        } catch (NotFoundException n) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
-    @DeleteMapping("/tasks/{id}")
-    public ResponseEntity<HttpStatus> deleteTask(@PathVariable("id") long id) {
+    @DeleteMapping("/tasks/{taskId}")
+    public ResponseEntity<Void> deleteTask(@PathVariable("taskId") long taskId) {
 		try {
-			taskService.deleteById(id);
-			return new ResponseEntity<>(HttpStatus.OK);
-		} catch (Exception e) {
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return TaskService.deleteTask(taskId);
+		} catch (NotFoundException n) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 }
